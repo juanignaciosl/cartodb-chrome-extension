f5a7504881e8929d305ba38dbb2e47fed88d8a6b
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2014-11-16 22:41:38 +0100

    Improved popup

--- src/cartodb-chrome-popup.js
+++ src/cartodb-chrome-popup.js
@@ -1,10 +1,25 @@
 function saveClicked() {
-  var apikey = document.getElementById('apikey').value;
-  chrome.storage.sync.set({'apikey': apikey }, null);
+  var apikey = document.getElementById('apikey');
+  if(apikey.value.length > 0) {
+    chrome.storage.sync.set({'apikey': apikey.value }, function() {
+      window.close();
+    });
+  } else {
+    alert('You must set the api key');
+  }
 }
 
 document.addEventListener(
     'DOMContentLoaded', 
     function () {
       document.getElementById("save-button").addEventListener('click', saveClicked);
+      document.getElementById('menu-image').src = chrome.extension.getURL("menu.png");
+      document.getElementById('logo-image').src = chrome.extension.getURL("cartodb.png");
     });
+
+window.addEventListener('click',function(e){
+    if(e.target.href!==undefined) {
+      chrome.tabs.create({url:e.target.href})
+    }
+});
+
